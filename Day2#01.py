74b765c3cdbeb247dce53cfc9fae77ac52672c25
Roger8886 <32452062+Roger8886@users.noreply.github.com>
2022-03-28 08:05:18 +0800

    複雜的字串取代
    #練習：請將字串當中的出現的 not … at all 換成 good ， 中間的「…」代表的是任意字串或沒有內容 。
    
    ❏ Requirements：
    
    1. 讓使用者輸入一個字串 s
    
    2. 利用程式將字串中出現的 not … at all 取代成 good 後輸出（Hint: 可以假設字串中最多只會出現一次 not ... at all）
    
    ❏ Sample Input #1: This company is not poor at all. ← 使用者輸入
    
    ❏ Sample Output #1: This company is good. ← 畫面輸出
    
    ❏ Sample Input #2: I'm not at all happy about it. ← 使用者輸入
    
    ❏ Sample Output #2: I'm good happy about it. ← 畫面輸出
    
    ❏ Sample Code:
    
    s = input() # This company is not poor at all. 
    
    # Your Code
    
    print(d) # This company is good.

--- Day2#01.py
+++
@@ -1,7 +0,0 @@
-import re
-
-input1 = "This company is not poor at all."
-print(re.sub("not.*at all", "good", input1))
-
-# input2 = "I'm not at all happy about it."
-# print(re.sub("not.*at all", "good", input2))
